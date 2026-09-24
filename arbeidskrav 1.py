3123880228d2fc4d29f452dbd6729d135ced0dd7
elizabethh-ux <ellizsuzi@yahoo.no>
2026-09-24 15:09:36 +0200

    Add files via upload

--- arbeidskrav 1.py
+++ arbeidskrav 1.py
@@ -15,13 +15,14 @@ print(strompris)
 forsikring_el = 5000  
 trafikkavgift = 8.38 * 365  # avgift er oppgitt hver dag, derfor ganger med 365 for å få årlig avgift
 print(trafikkavgift)
-bom_el = km * 0.10  # bomavgiften for antall km elbil, ganger km med 0.1 for å finne total bomacgift på 10000km 
+bom_el = km * 0.10  # bomavgiften for antall km elbil, ganger km med 0.1 for å finne total bomavgift på 10000km 
 print(bom_el)
-total_el = strompris + forsikring_el + trafikkavgift + bom_el  # plusser sammen alle kostbader for totalkostnader elbil 
+total_el = strompris + forsikring_el + trafikkavgift + bom_el  # plusser sammen alle kostnader for totalkostnader elbil 
 print("Totalkostnad elbil per år:" ,total_el)
 
   # Beregning av totalkostnad for bensinbil
   # Her er det gjort samme beregninger som for elbil, bruker samme trafikkavgift som for elbil da kostnaden er lik 
+  
 drivstoff = km * 1  # her ganget med 1, da dette er pris for bensin per km for å finne totalavgift 
 print(drivstoff)
 forsikring_bensin = 7500
@@ -33,3 +34,5 @@ print("Totalkostnad bensinbil per år:" ,total_bensin)
   # finne forskjell i avgift per år for elbil og bensinbil 
 forskjell = total_bensin - total_el  # totalekostnader for bensinbil minus totale kostnader for elbil
 print("Differansen årlige kostnader:" ,forskjell)
+
+  # Vi kan da konkludere med at det er 10 500 kr billigere per 10 000 km med elbil 
